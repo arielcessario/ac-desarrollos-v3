2caacc192bd01dcb6031f0d1429fc0cfacaa80c8
Mateo Maneff <mmaneff@gmail.com>
2016-02-10 09:46:29 -0200

    Slider y noticias

--- app/hacemos/hacemos.js
+++ app/hacemos/hacemos.js
@@ -12,9 +12,11 @@
     function HacemosController($scope, $interval) {
         var vm = this;
 
-        vm.slider_01 = {titulo: 'Ecommerce, Mcommerce y Comercio Social:', descripcion:'El comercio electronico, tambien conocido como e-commerce (electronic commerce en inglés), consiste en la compra y venta de productos o de servicios a traves de medios electronicos. La mayor parte del comercio electronico consiste en la compra y venta de productos o servicios entre personas y empresas, sin embargo un porcentaje considerable del comercio electrónico consiste en la adquisición de artículos virtuales (software y derivados en su mayoría), tales como el acceso a contenido "premium" de un sitio web.'};
-        vm.slider_02 = {titulo: 'Institucionales', descripcion:'los sitios web institucionales'};
-        vm.slider_03 = {titulo: 'Sistema de Gestion', descripcion:'Un sistema robusto y completo'};
+        vm.slider_01 = {titulo: '', descripcion:''};
+        vm.slider_02 = {titulo: '', descripcion:''};
+        vm.slider_03 = {titulo: '', descripcion:''};
+        vm.slider_04 = {titulo: '', descripcion:''};
+        vm.slider_05 = {titulo: '', descripcion:''};
 
         vm.slider_nro = 1;
         //vm.slideSelected = 1;
@@ -22,7 +24,7 @@
         $interval(changeSlider, 5000);
 
         function changeSlider() {
-            vm.slider_nro = (vm.slider_nro == 3) ? 1 : vm.slider_nro + 1;
+            vm.slider_nro = (vm.slider_nro == 5) ? 1 : vm.slider_nro + 1;
         }
     };
 
--- app/sabias/sabias.js
+++ app/sabias/sabias.js
@@ -7,12 +7,24 @@
     angular.module('acdesarrollos.sabias', ['ngRoute'])
         .controller('SabiasController', SabiasController);
 
-    SabiasController.$inject = ['$scope', '$location'];
+    SabiasController.$inject = ['$scope', '$interval'];
 
-    function SabiasController($scope, $location) {
+    function SabiasController($scope, $interval) {
         var vm = this;
 
+        vm.slider_01 = {titulo: '', descripcion:''};
+        vm.slider_02 = {titulo: '', descripcion:''};
+        vm.slider_03 = {titulo: '', descripcion:''};
+        vm.slider_04 = {titulo: '', descripcion:''};
+
         vm.slider_nro = 1;
+
+        $interval(changeSlider, 5000);
+
+        function changeSlider() {
+            vm.slider_nro = (vm.slider_nro == 4) ? 1 : vm.slider_nro + 1;
+        }
+
     };
 
 })();
