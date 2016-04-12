f71f07b6701956c158b58d1d24cdc170e027609c
Mateo Maneff <mmaneff@gmail.com>
2016-04-12 10:14:36 -0300

    Adwords y user component para el slider de hacemos.

--- app/contactanos/contacto.js
+++ app/contactanos/contacto.js
@@ -6,17 +6,20 @@
     var currentScriptPath = scripts[scripts.length - 1].src;
 
     angular.module('acdesarrollos.contacto', ['ngRoute'])
-        .controller('ContactoController', ContactoController)
+        .controller('ContactoController', ContactoController);
+    /*
         .constant('googleAdsContact', {
             google_conversion_id: 956728168,
             google_conversion_label: "_8PjCNaT810Q6IaayAM",
             google_conversion_format: "3",
             google_remarketing_only: false
         });
+*/
+    //ContactoController.$inject = ['$scope', 'ContactsService', '$location', '$window', 'googleAdsContact'];
+    ContactoController.$inject = ['$scope', 'ContactsService', '$location'];
 
-    ContactoController.$inject = ['$scope', 'ContactsService', '$location', '$window', 'googleAdsContact'];
-
-    function ContactoController($scope, ContactsService, $location, $window, googleAdsContact) {
+    //function ContactoController($scope, ContactsService, $location, $window, googleAdsContact) {
+    function ContactoController($scope, ContactsService, $location) {
         var vm = this;
 
         vm.email = '';
@@ -36,7 +39,7 @@
             }
             vm.enviando = true;
 
-            $window.google_trackConversion(googleAdsContact);
+            //$window.google_trackConversion(googleAdsContact);
 
             ContactsService.sendMail(vm.email,
                 [{mail: 'arielcessario@gmail.com'}, {mail: 'mmaneff@gmail.com'}, {mail: 'diegoyankelevich@gmail.com'}],
@@ -52,7 +55,6 @@
                     vm.mensaje = '';
                 });
         }
-
 
     };
 
--- app/hacemos/hacemos.js
+++ app/hacemos/hacemos.js
@@ -5,6 +5,26 @@
     var currentScriptPath = scripts[scripts.length - 1].src;
 
     angular.module('acdesarrollos.hacemos', ['ngRoute'])
+        .component('sliderEcommerce', {
+            controllerAs: 'hacemosCtrl',
+            templateUrl: "hacemos/hacemos/ecommerce.html"
+        })
+        .component('sliderInstitucional', {
+            controllerAs: 'hacemosCtrl',
+            templateUrl: "hacemos/hacemos/institucionales.html"
+        })
+        .component('sliderSistema', {
+            controllerAs: 'hacemosCtrl',
+            templateUrl: "hacemos/hacemos/sistemas.html"
+        })
+        .component('sliderDesarrollo', {
+            controllerAs: 'hacemosCtrl',
+            templateUrl: "hacemos/hacemos/desarrollos.html"
+        })
+        .component('sliderAnalitica', {
+            controllerAs: 'hacemosCtrl',
+            templateUrl: "hacemos/hacemos/analiticas.html"
+        })
         .controller('HacemosController', HacemosController);
 
     HacemosController.$inject = ['$scope', '$interval', '$location'];
@@ -12,49 +32,57 @@
     function HacemosController($scope, $interval, $location) {
         var vm = this;
 
+        vm.view = 'hacemos/hacemos/ecommerce.html';
+        vm.slider_nro = 1;
+
         vm.slider_01 = {titulo: '', descripcion:''};
         vm.slider_02 = {titulo: '', descripcion:''};
         vm.slider_03 = {titulo: '', descripcion:''};
         vm.slider_04 = {titulo: '', descripcion:''};
         vm.slider_05 = {titulo: '', descripcion:''};
 
-        vm.slider_nro = 1;
-        //vm.slideSelected = 1;
-
         vm.showSlider = showSlider;
 
         $interval(changeSlider, 30000);
+        //$interval(changeSlider, 5000);
 
         function changeSlider() {
             vm.slider_nro = (vm.slider_nro == 5) ? 1 : vm.slider_nro + 1;
 
-            if(vm.slider == 1) {
-                $location.path('/hacemos/ecommerce');
-            } else if(vm.slider == 2) {
-                $location.path('/hacemos/institucionales');
-            } else if(vm.slider == 3) {
-                $location.path('/hacemos/sistemas');
-            } else if(vm.slider == 4) {
-                $location.path('/hacemos/desarrollos');
-            } else if(vm.slider == 5) {
-                $location.path('/hacemos/analiticas');
+            if(vm.slider_nro == 1) {
+                vm.view = 'hacemos/hacemos/ecommerce.html';
+            } else if(vm.slider_nro == 2) {
+                vm.view = 'hacemos/hacemos/institucionales.html';
+            } else if(vm.slider_nro == 3) {
+                vm.view = 'hacemos/hacemos/sistemas.html';
+            } else if(vm.slider_nro == 4) {
+                vm.view = 'hacemos/hacemos/desarrollos.html';
+            } else if(vm.slider_nro == 5) {
+                vm.view = 'hacemos/hacemos/analiticas.html';
             }
+            //console.log(vm.view);
+            //console.log($location);
+            //console.log($location.path());
+            //console.log($location.absUrl());
         }
 
         function showSlider(slider) {
             vm.slider_nro = slider;
 
             if(slider == 1) {
-                $location.path('/hacemos/ecommerce');
+                vm.view = 'hacemos/hacemos/ecommerce.html';
+                //$location.path('hacemos/ecommerce.html');
             } else if(slider == 2) {
-                $location.path('/hacemos/institucionales');
+                vm.view = 'hacemos/hacemos/institucionales.html';
             } else if(slider == 3) {
-                $location.path('/hacemos/sistemas');
+                vm.view = 'hacemos/hacemos/sistemas.html';
             } else if(slider == 4) {
-                $location.path('/hacemos/desarrollos');
+                vm.view = 'hacemos/hacemos/desarrollos.html';
             } else if(slider == 5) {
-                $location.path('/hacemos/analiticas');
+                vm.view = 'hacemos/hacemos/analiticas.html';
             }
+            //console.log(vm.view);
+            //console.log($location);
         }
     };
 
